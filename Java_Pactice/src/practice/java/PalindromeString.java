62ad89c8db932c007756a947f629248ea047d71b
upasa <upasa@Sana-PC>
2025-09-24 20:25:21 +0530

    updated

--- Java_Pactice/src/practice/java/PalindromeString.java
+++ Java_Pactice/src/practice/java/PalindromeString.java
@@ -10,20 +10,11 @@ public class PalindromeString {
 		for (int i = 0; i < n / 2; i++) {
 			int index = (n - 1) - i;
 			if (str.charAt(i) != str.charAt(index)) {
-//				flag = 1;
-//				continue;
 				return false;
 			}
-//			} else {
-//				flag = 0;
-//			}
+
 		}
 		return true;
-//		if (flag == 1)
-//			System.out.println(str + " " + "is a palindrome");
-//
-//		else if (flag == 0)
-//			System.out.println(str + " " + "is not a palindrome");
 	}
 
 	public static void main(String[] args) {
